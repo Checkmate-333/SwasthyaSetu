77fa79e70f98035dc8ad8d1ff2312ae20cf61d8b
Checkmate-333 <kuntal.biswas.0698@gmail.com>
2026-05-07 20:59:01 +0530

    Update MongoDB connection string placeholder

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gramarogya');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'Mongodb url');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error connecting to MongoDB: ${err.message}`);
